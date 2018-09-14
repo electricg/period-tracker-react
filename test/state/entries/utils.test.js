a3204cf02632111aac4c86ced3b53d32d95bc913
Giulia Alfonsi <electric.g@gmail.com>
2018-09-13 22:58:39 -0700

    More reusable tests for utils

--- test/state/entries/utils.test.js
+++ test/state/entries/utils.test.js
@@ -1,12 +1,7 @@
 const {
   getToday,
   newDate,
-  sortDates,
-  deleteDuplicatedDates,
-  doesDateExists,
-  calcDatesRanges,
-  calcDatesCovered,
-  calcFutureDates,
+  ...utils
 } = require('../../../src/state/entries/utils');
 
 describe('getToday', () => {
@@ -100,383 +95,327 @@ describe('newDate', () => {
   });
 });
 
-describe('sortDates', () => {
-  const cases = [
+const methods = Object.keys(utils);
+
+const _cases = {
+  sortDates: [
     {
       desc: 'should succeed with null',
-      input: null,
+      input: [null],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with undefined',
-      input: undefined,
+      input: [undefined],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a string',
-      input: 'test',
+      input: ['test'],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a number',
-      input: 0,
+      input: [0],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a bool',
-      input: true,
+      input: [true],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an object',
-      input: {},
+      input: [{}],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a function',
-      input: () => {},
+      input: [() => {}],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an empty array',
-      input: [],
+      input: [[]],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an array of one item',
-      input: ['2018-01-01'],
+      input: [['2018-01-01']],
       expectedOutput: ['2018-01-01'],
     },
     {
       desc: 'should succeed with an array of multiple items',
-      input: ['2018-02-01', '2018-01-02', '2018-01-01'],
+      input: [['2018-02-01', '2018-01-02', '2018-01-01']],
       expectedOutput: ['2018-01-01', '2018-01-02', '2018-02-01'],
     },
     {
       desc:
         'should succeed with an array of multiple items and some identical items',
-      input: ['2018-02-01', '2018-01-02', '2018-01-02', '2018-01-01'],
+      input: [['2018-02-01', '2018-01-02', '2018-01-02', '2018-01-01']],
       expectedOutput: ['2018-01-01', '2018-01-02', '2018-01-02', '2018-02-01'],
     },
-  ];
-
-  cases.forEach(o => {
-    const { desc, input, expectedOutput } = o;
-    test(desc, () => {
-      const output = sortDates(input);
-      expect(output).toEqual(expectedOutput);
-    });
-  });
-});
-
-describe('deleteDuplicatedDates', () => {
-  const cases = [
+  ],
+  deleteDuplicatedDates: [
     {
       desc: 'should succeed with null',
-      input: null,
+      input: [null],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with undefined',
-      input: undefined,
+      input: [undefined],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a string',
-      input: 'test',
+      input: ['test'],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a number',
-      input: 0,
+      input: [0],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a bool',
-      input: true,
+      input: [true],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an object',
-      input: {},
+      input: [{}],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a function',
-      input: () => {},
+      input: [() => {}],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an empty array',
-      input: [],
+      input: [[]],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an array of one item',
-      input: ['2018-01-01'],
+      input: [['2018-01-01']],
       expectedOutput: ['2018-01-01'],
     },
     {
       desc:
         'should succeed with an array of multiple items and no identical items',
-      input: ['2018-02-01', '2018-01-02', '2018-01-01'],
+      input: [['2018-02-01', '2018-01-02', '2018-01-01']],
       expectedOutput: ['2018-02-01', '2018-01-02', '2018-01-01'],
     },
     {
       desc:
         'should succeed with an array of multiple items and some identical items',
-      input: ['2018-02-01', '2018-01-02', '2018-01-01', '2018-01-02'],
+      input: [['2018-02-01', '2018-01-02', '2018-01-01', '2018-01-02']],
       expectedOutput: ['2018-02-01', '2018-01-02', '2018-01-01'],
     },
-  ];
-
-  cases.forEach(o => {
-    const { desc, input, expectedOutput } = o;
-    test(desc, () => {
-      const output = deleteDuplicatedDates(input);
-      expect(output).toEqual(expectedOutput);
-    });
-  });
-});
-
-describe('doesDateExists', () => {
-  const cases = [
+  ],
+  doesDateExists: [
     {
       desc: 'should return false with null',
-      input1: '',
-      input2: null,
+      input: ['', null],
       expectedOutput: false,
     },
     {
       desc: 'should return false with undefined',
-      input1: '',
-      input2: undefined,
+      input: ['', undefined],
       expectedOutput: false,
     },
     {
       desc: 'should return false with a string',
-      input1: '',
-      input2: 'test',
+      input: ['', 'test'],
       expectedOutput: false,
     },
     {
       desc: 'should return false with a number',
-      input1: '',
-      input2: 0,
+      input: ['', 0],
       expectedOutput: false,
     },
     {
       desc: 'should return false with a bool',
-      input1: '',
-      input2: true,
+      input: ['', true],
       expectedOutput: false,
     },
     {
       desc: 'should return false with an object',
-      input1: '',
-      input2: {},
+      input: ['', {}],
       expectedOutput: false,
     },
     {
       desc: 'should return false with a function',
-      input1: '',
-      input2: () => {},
+      input: ['', () => {}],
       expectedOutput: false,
     },
     {
       desc: 'should return false with an empty array',
-      input1: ['2018-01-01'],
-      input2: [],
+      input: [['2018-01-01'], []],
       expectedOutput: false,
     },
     {
       desc:
         'should return false when element is not found in an array of one item',
-      input1: '2018-01-02',
-      input2: ['2018-01-01'],
+      input: ['2018-01-02', ['2018-01-01']],
       expectedOutput: false,
     },
     {
       desc: 'should return true when element is found in an array of one item',
-      input1: '2018-01-01',
-      input2: ['2018-01-01'],
+      input: ['2018-01-01', ['2018-01-01']],
       expectedOutput: true,
     },
     {
       desc:
         'should return true when element is found in an array of multiple items and no identical items',
-      input1: '2018-01-01',
-      input2: ['2018-02-01', '2018-01-02', '2018-01-01'],
+      input: ['2018-01-01', ['2018-02-01', '2018-01-02', '2018-01-01']],
       expectedOutput: true,
     },
     {
       desc:
         'should return true when element is found in an array of multiple items and some identical items',
-      input1: '2018-01-01',
-      input2: ['2018-02-01', '2018-01-02', '2018-01-01', '2018-01-02'],
+      input: [
+        '2018-01-01',
+        ['2018-02-01', '2018-01-02', '2018-01-01', '2018-01-02'],
+      ],
       expectedOutput: true,
     },
-  ];
-
-  cases.forEach(o => {
-    const { desc, input1, input2, expectedOutput } = o;
-    test(desc, () => {
-      const output = doesDateExists(input1, input2);
-      expect(output).toEqual(expectedOutput);
-    });
-  });
-});
-
-describe('calcDatesRanges', () => {
-  const cases = [
+  ],
+  calcDatesRanges: [
     {
       desc: 'should succeed with null',
-      input: null,
+      input: [null],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with undefined',
-      input: undefined,
+      input: [undefined],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a string',
-      input: 'test',
+      input: ['test'],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a number',
-      input: 0,
+      input: [0],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a bool',
-      input: true,
+      input: [true],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an object',
-      input: {},
+      input: [{}],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a function',
-      input: () => {},
+      input: [() => {}],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an empty array',
-      input: [],
+      input: [[]],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an array of one item',
-      input: ['2018-01-01'],
+      input: [['2018-01-01']],
       expectedOutput: [0],
     },
     {
       desc: 'should succeed with an array of two identical items',
-      input: ['2018-01-01', '2018-01-01'],
+      input: [['2018-01-01', '2018-01-01']],
       expectedOutput: [0, 0],
     },
     {
       desc: 'should succeed with an array of two different items',
-      input: ['2018-01-01', '2018-01-02'],
+      input: [['2018-01-01', '2018-01-02']],
       expectedOutput: [0, 1],
     },
     {
       desc: 'should succeed with an array of multiple different items',
-      input: ['2018-01-01', '2018-01-02', '2018-02-01', '2019-02-01'],
+      input: [['2018-01-01', '2018-01-02', '2018-02-01', '2019-02-01']],
       expectedOutput: [0, 1, 30, 365],
     },
     {
       desc:
         'should succeed with an array of multiple different items in desc order',
-      input: ['2019-02-01', '2018-02-01', '2018-01-02', '2018-01-01'],
+      input: [['2019-02-01', '2018-02-01', '2018-01-02', '2018-01-01']],
       expectedOutput: [0, 365, 30, 1],
     },
-  ];
-
-  cases.forEach(o => {
-    const { desc, input, expectedOutput } = o;
-    test(desc, () => {
-      const output = calcDatesRanges(input);
-      expect(output).toEqual(expectedOutput);
-    });
-  });
-});
-
-describe('calcDatesCovered', () => {
-  const cases = [
+  ],
+  calcDatesCovered: [
     {
       desc: 'should succeed with null',
-      input1: null,
+      input: [null],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with undefined',
-      input1: undefined,
+      input: [undefined],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a string',
-      input1: 'test',
+      input: ['test'],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a number',
-      input1: 0,
+      input: [0],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a bool',
-      input1: true,
+      input: [true],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an object',
-      input1: {},
+      input: [{}],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a function',
-      input1: () => {},
+      input: [() => {}],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with an empty array',
-      input1: [],
+      input: [[]],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with a cover equal to 0',
-      input1: ['2019-02-01', '2018-02-01', '2018-01-02', '2018-01-01'],
-      input2: 0,
+      input: [['2019-02-01', '2018-02-01', '2018-01-02', '2018-01-01'], 0],
       expectedOutput: ['2019-02-01', '2018-02-01', '2018-01-02', '2018-01-01'],
     },
     {
       desc: 'should succeed with a negative cover',
-      input1: ['2019-02-01', '2018-02-01', '2018-01-02', '2018-01-01'],
-      input2: -1,
+      input: [['2019-02-01', '2018-02-01', '2018-01-02', '2018-01-01'], -1],
       expectedOutput: ['2019-02-01', '2018-02-01', '2018-01-02', '2018-01-01'],
     },
     {
       desc: 'should succeed with an array of one item and cover equal to 1',
-      input1: ['2018-01-01'],
-      input2: 1,
+      input: [['2018-01-01'], 1],
       expectedOutput: ['2018-01-01', '2018-01-02'],
     },
     {
       desc: 'should succeed with an array of one item and cover equal to 2',
-      input1: ['2018-01-30'],
-      input2: 2,
+      input: [['2018-01-30'], 2],
       expectedOutput: ['2018-01-30', '2018-01-31', '2018-02-01'],
     },
     {
       desc:
         'should succeed with an array of multiple different items and cover equal to 3',
-      input1: ['2018-01-01', '2018-01-02', '2018-02-01', '2018-12-30'],
-      input2: 3,
+      input: [['2018-01-01', '2018-01-02', '2018-02-01', '2018-12-30'], 3],
       expectedOutput: [
         '2018-01-01',
         '2018-01-02',
@@ -496,48 +435,31 @@ describe('calcDatesCovered', () => {
         '2019-01-02',
       ],
     },
-  ];
-
-  cases.forEach(o => {
-    const { desc, input1, input2, expectedOutput } = o;
-    test(desc, () => {
-      const output = calcDatesCovered(input1, input2);
-      expect(output).toEqual(expectedOutput);
-    });
-  });
-});
-
-describe('calcFutureDates', () => {
-  const cases = [
+  ],
+  calcFutureDates: [
     {
       desc: 'should succeed with no lastDate',
-      input1: '',
+      input: [''],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with no untilDate',
-      input1: '2018-01-01',
-      input2: '',
+      input: ['2018-01-01', ''],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with range equal to 0',
-      input1: '2018-01-01',
-      input2: '2018-02-01',
-      input3: 0,
+      input: ['2018-01-01', '2018-02-01', 0],
       expectedOutput: [],
     },
     {
       desc: 'should succeed with no range passed',
-      input1: '2017-12-04',
-      input2: '2018-02-01',
+      input: ['2017-12-04', '2018-02-01'],
       expectedOutput: ['2018-01-01', '2018-01-29', '2018-02-26'],
     },
     {
       desc: 'should succeed with no range equal to 1',
-      input1: '2017-12-27',
-      input2: '2018-01-04',
-      input3: 1,
+      input: ['2017-12-27', '2018-01-04', 1],
       expectedOutput: [
         '2017-12-28',
         '2017-12-29',
@@ -550,13 +472,23 @@ describe('calcFutureDates', () => {
         '2018-01-05',
       ],
     },
-  ];
+  ],
+};
 
-  cases.forEach(o => {
-    const { desc, input1, input2, input3, expectedOutput } = o;
-    test(desc, () => {
-      const output = calcFutureDates(input1, input2, input3);
-      expect(output).toEqual(expectedOutput);
+methods.forEach(method => {
+  describe(method, () => {
+    const cases = _cases[method];
+    cases.forEach(o => {
+      const { desc, input, expectedOutput } = o;
+      const freeze = input.map(item => JSON.stringify(item));
+      test(desc, () => {
+        const output = utils[method].apply(null, input);
+        expect(output).toEqual(expectedOutput);
+        // check that the function is pure and doesn't change the arguments
+        input.forEach((item, i) => {
+          expect(JSON.stringify(item)).toEqual(freeze[i]);
+        });
+      });
     });
   });
 });
