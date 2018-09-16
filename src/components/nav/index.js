dbb1ab4fcb78d40d378f2d574446fb33d85c2002
Giulia Alfonsi <electric.g@gmail.com>
2018-09-15 22:40:33 -0700

    Better nav

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,21 +1,55 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Nav = () => {
+import './nav.css';
+
+const NavItem = ({ path = '', label = '', exact = false }) => {
+  if (!path || !label) {
+    return null;
+  }
+
   return (
-    <nav>
-      <NavLink exact to="/">
-        home
+    <li className="main-nav__item">
+      <NavLink
+        className="main-nav__link"
+        activeClassName="main-nav__link--selected"
+        to={path}
+        exact={exact}
+      >
+        {label}
       </NavLink>
-      <NavLink exact to="/calendar">
-        calendar
-      </NavLink>
-      <NavLink exact to="/log">
-        log
-      </NavLink>
-      <NavLink exact to="/settings">
-        settings
-      </NavLink>
+    </li>
+  );
+};
+
+const Nav = () => {
+  const nav = [
+    {
+      path: '/',
+      label: 'home',
+      exact: true,
+    },
+    {
+      path: '/calendar',
+      label: 'calendar',
+    },
+    {
+      path: '/log',
+      label: 'log',
+    },
+    {
+      path: '/settings',
+      label: 'settings',
+    },
+  ];
+
+  return (
+    <nav className="main-nav">
+      <ul className="main-nav__list">
+        {nav.map(item => (
+          <NavItem key={item.path} {...item} />
+        ))}
+      </ul>
     </nav>
   );
 };
