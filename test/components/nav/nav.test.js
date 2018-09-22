1fbca2c3947ea43d69ba00d4cbb9c2358ba46672
Giulia Alfonsi <electric.g@gmail.com>
2018-09-22 12:02:45 -0700

    First snapshot tests

---
+++ test/components/nav/nav.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { StaticRouter } from 'react-router';
+import Nav from '../../../src/components/nav';
+
+test('should succeed with home page', () => {
+  const component = renderer.create(
+    <StaticRouter location="/" context={{}}>
+      <Nav />
+    </StaticRouter>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should succeed with calendar page', () => {
+  const component = renderer.create(
+    <StaticRouter location="/calendar" context={{}}>
+      <Nav />
+    </StaticRouter>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should succeed with calendar sub page', () => {
+  const component = renderer.create(
+    <StaticRouter location="/calendar/2018" context={{}}>
+      <Nav />
+    </StaticRouter>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should succeed with log page', () => {
+  const component = renderer.create(
+    <StaticRouter location="/log" context={{}}>
+      <Nav />
+    </StaticRouter>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should succeed with settings page', () => {
+  const component = renderer.create(
+    <StaticRouter location="/settings" context={{}}>
+      <Nav />
+    </StaticRouter>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
